a5c32dfc4f3f588ef6f87861800c99cfe717c075
anas-yagoub <anasyagoub7@gmail.com>
2023-09-07 14:34:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-        char myChar[] = "Programming is like building a multilingual puzzle\n";
+        char myChar[] = ""Programming is like building a multilingual puzzle\n";
 	puts(myChar);
 	return(0);
 }
