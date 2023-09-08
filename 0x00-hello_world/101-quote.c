17686ae667a7efe848bc117f4d3d255aec50aeb8
anas-yagoub <anasyagoub7@gmail.com>
2023-09-08 03:16:38 -0700

    qu

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	char qua[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, qua, 59);
 	return (1);
 }
