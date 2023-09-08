b020a268b2a338104b31c7f4adcbc6a95f13105f
anas-yagoub <anasyagoub7@gmail.com>
2023-09-08 15:32:23 -0700

    print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a serie of numbers with commas
+  *
+  * Return: Always (Success);
+  */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		else
+		{
+			putchar('$');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
